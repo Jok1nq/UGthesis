a2f6e00c2b23cb9a9090dc228bf52e6186cc6708
Jok1nq <jokinqz@gmail.com>
2024-05-14 12:56:03 +0800

    modified:   Readme/README.md 	modified:   body/ref.bib 	modified:   body/undergraduate/final/1-introduction.tex 	modified:   xelatex.py 	new file:   zjuthesis.aux 	new file:   zjuthesis.bbl 	new file:   zjuthesis.bcf 	new file:   zjuthesis.blg 	new file:   zjuthesis.log 	new file:   zjuthesis.pdf 	new file:   zjuthesis.run.xml 	modified:   zjuthesis.tex 	new file:   zjuthesis.toc

--- xelatex.py
+++ xelatex.py
@@ -2,5 +2,6 @@ import os
 
 # 编译
 os.system("xelatex -f zjuthesis.tex")
-os.system("latexmk -c")
-os.system("move .\\zjuthesis.pdf  ..\\ ")
+os.system("biber zjuthesis")
+os.system("xelatex -f zjuthesis.tex")
+os.system("xelatex -f zjuthesis.tex")
